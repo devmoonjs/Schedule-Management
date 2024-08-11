6cce1959e157c78afd6dbccc4cb296940be6781c
MOON <moonjs1018@naver.com>
2024-08-11 21:02:52 +0900

    add : 3 Layer Architecture 환경 설정

---
+++ src/main/java/com/sparta/schedulemanagement/entity/Schedule.java
@@ -0,0 +1,12 @@
+package com.sparta.schedulemanagement.entity;
+
+import java.sql.Timestamp;
+
+public class Schedule {
+    int id;
+    String content;
+    String name;
+    String password;
+    Timestamp createDate;
+    Timestamp modifyDate;
+}
---
+++ src/main/java/com/sparta/schedulemanagement/repository/ScheduleRepository.java
@@ -0,0 +1,4 @@
+package com.sparta.schedulemanagement.repository;
+
+public class ScheduleRepository {
+}
